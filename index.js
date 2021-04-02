acd2c11d6adee8e89b7d013aa01a11435a3fcc43
sudzzz <sudhirdaga1998@gmail.com>
2021-04-02 22:49:19 +0530

    Handling HTTP DELETE Request

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const courses = [
     {
         id : 3,
         name : "course3"
-    }
+    },
 ]
 
 app.get('/',(req,res)=>{
@@ -29,9 +29,7 @@ app.get('/api/courses',(req,res)=>{
 
 app.get('/api/courses/:id',(req,res)=>{
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course){
-        res.status(404).send("The course with given id is not found");
-    }
+    if(!course) return res.status(404).send("The course with given id is not found");
     res.send(course);
 });
 
@@ -54,11 +52,7 @@ app.post('/api/courses',(req,res)=>{
     }*/
     const { error } = validateCourse(req.body);//Equivalent to result.error
 
-    if(error)
-    {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
+    if(error) return res.status(400).send(error.details[0].message);
     const course = {
         id : courses.length + 1,
         name : req.body.name
@@ -72,9 +66,7 @@ app.put('/api/courses/:id',(req,res)=>{
     //Look up the course
     //If not existing, return 404 - Not Found
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course){
-        res.status(404).send("The course with given id is not found");
-    }
+    if(!course) return res.status(404).send("The course with given id is not found");
     
     //Validate
     //If invalid, return 400 - Bad Request
@@ -87,11 +79,7 @@ app.put('/api/courses/:id',(req,res)=>{
     const { error } = validateCourse(req.body);//Equivalent to result.error
 
 
-    if(error)
-    {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
+    if(error) return res.status(400).send(error.details[0].message);
 
     //Update course
     //Return the updated course
@@ -99,6 +87,20 @@ app.put('/api/courses/:id',(req,res)=>{
     res.send(course);
 
 });
+
+app.delete('/api/courses/:id',(req,res)=>{
+    //Look up the course
+    //Not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send("The course with given id is not found"); 
+
+    //Delete
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    //Return the same course
+    res.send(course);
+})
 
 function validateCourse(course){
     const schema = {
@@ -112,3 +114,4 @@ const port = process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 });
+
