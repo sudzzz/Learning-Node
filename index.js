8273ad979ae61d333ed1728464e357d73e56dd65
sudzzz <sudhirdaga1998@gmail.com>
2021-04-02 22:14:51 +0530

    Handling HTTP PUT Request

--- index.js
+++ index.js
@@ -36,20 +36,27 @@ app.get('/api/courses/:id',(req,res)=>{
 });
 
 app.post('/api/courses',(req,res)=>{
-    const schema = {
+   /* const schema = {
         name : Joi.string().min(3).required()
     }
     const result = Joi.validate(req.body,schema);
 
     //console.log(result);
 
-    /*if(!req.body.name || req.body.name.length<3){
+    if(!req.body.name || req.body.name.length<3){
         //400 Bad Request
         res.status(400).send("Name is required and should be minimum 3 charachters");
         return;
-    }*/
+    }
     if(result.error){
         res.status(400).send(result.error.details[0].message);
+        return;
+    }*/
+    const { error } = validateCourse(req.body);//Equivalent to result.error
+
+    if(error)
+    {
+        res.status(400).send(error.details[0].message);
         return;
     }
     const course = {
@@ -59,6 +66,46 @@ app.post('/api/courses',(req,res)=>{
     courses.push(course);
     res.send(course);
 });
+
+
+app.put('/api/courses/:id',(req,res)=>{
+    //Look up the course
+    //If not existing, return 404 - Not Found
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course){
+        res.status(404).send("The course with given id is not found");
+    }
+    
+    //Validate
+    //If invalid, return 400 - Bad Request
+    /*const schema = {
+        name : Joi.string().min(3).required()
+    }
+    const result = Joi.validate(req.body,schema)*/
+
+    //const result = validateCourse(req.body);
+    const { error } = validateCourse(req.body);//Equivalent to result.error
+
+
+    if(error)
+    {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    //Update course
+    //Return the updated course
+    course.name = req.body.name;
+    res.send(course);
+
+});
+
+function validateCourse(course){
+    const schema = {
+        name : Joi.string().min(3).required()
+    };
+    return Joi.validate(course,schema)
+}
 
 //PORT
 const port = process.env.PORT || 3000;
