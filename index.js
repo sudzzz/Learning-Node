76e72b742c8e75ee39d511f62029d6cc2dbebaa9
sudzzz <sudhirdaga1998@gmail.com>
2021-04-03 23:54:55 +0530

    Async and Await

--- index.js
+++ index.js
@@ -17,12 +17,20 @@ console.log("Before");
     )
 );*/
 
-getUser(1)
+/*getUser(1)
   .then(user => getRepositories(user.gitHubUsername))
   .then(repos => getCommits(repos[0]))
   .then(commits => console.log("Commits",commits))
-  .catch(err => console.log("Error",err.message));
+  .catch(err => console.log("Error",err.message));*/
 
+//Async and Await
+async function displayCommits(){
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  }
+  displayCommits()
 
 console.log('After');
 
