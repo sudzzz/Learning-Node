6d9affe29046b97a778aabcce660cd46abf7d3c0
sudzzz <sudhirdaga1998@gmail.com>
2021-04-03 19:02:39 +0530

    Callback-Hell

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+//Asynchronus Code
+
 console.log("Before");
 getUser(1,(user)=>{
+    //console.log("User ",user);
     const name = user.gitHubUsername;
-    getRepositories(name,(repo)=>{
-        console.log("Repositories ",repo);
+    getRepositories(name,(repos)=>{
+        //console.log("Repositories ",repos);
+        const repo = repos[0];
+        getCommits(repo,(commits)=>{
+            //CALLBACK HELL
+            console.log("Commits",commits);
+        });
     });
 });
 console.log('After');
+
+
+//Synchronus Code
+console.log("Before");
+const user = getUser(1);
+const repos = getRepositories(user.gitHubUsername);
+const commits = getCommits(repos[0]);
+console.log(commits);
+console.log("After");
+
+
 
 function getUser(id,callback){
     setTimeout(()=>{
@@ -18,5 +37,12 @@ function getRepositories(username,callback){
     setTimeout(()=>{
         console.log("Fetching repositories from github...")
         callback(['repo1','repo2','repo3']);
+    },2000);
+}
+
+function getCommits(repo,callback){
+    setTimeout(()=>{
+        console.log("Fetching commits from github repositorty...")
+        callback(['commit1','commit2','commit3']);
     },2000);
 }
