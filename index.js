23ab3d94f5951bf4c905574dd88a130054ee1d27
sudzzz <sudhirdaga1998@gmail.com>
2021-04-03 18:51:10 +0530

    Callbacks

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-//Asynchronus-Code
-
 console.log("Before");
-setTimeout(()=>{
-    console.log("Reading a user form database...");
-},2000);
+getUser(1,(user)=>{
+    const name = user.gitHubUsername;
+    getRepositories(name,(repo)=>{
+        console.log("Repositories ",repo);
+    });
+});
 console.log('After');
 
-/* Output 
-    Before
-    After
-    Reading a user form database...//after 2 seconds
-*/
+function getUser(id,callback){
+    setTimeout(()=>{
+        console.log("Reading a user form database...");
+        callback({id : id, gitHubUsername : "sudzzz"});
+    },2000);
+}
 
-//Synchronus-Code
-
-console.log("Before");
-console.log('After');
-
-/* Output 
-    Before
-    After
-*/
+function getRepositories(username,callback){
+    setTimeout(()=>{
+        console.log("Fetching repositories from github...")
+        callback(['repo1','repo2','repo3']);
+    },2000);
+}
