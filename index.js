bfab0f79463c828d75f825d9076e0a68d7ec42d9
sudzzz <sudhirdaga1998@gmail.com>
2021-04-04 18:46:08 +0530

    Read(relational-operations)

--- index.js
+++ index.js
@@ -43,6 +43,11 @@ async function getCourses(){
     nin (not in)
     */
 
+    //Logical Operators
+    /*
+    or
+    and
+     */
 
     //Display every document
     const courses = await Course.find();
@@ -52,7 +57,10 @@ async function getCourses(){
     const course2 = await Course
         //.find({author : "Mosh", isPublished : true})
         //.find({price : {$gte : 10, $lte : 20}})//selects book with price between 10 and 20
-        .find({price : { $in : [10,15,20] } })// select book with price either 10,15 or 20
+        //.find({price : { $in : [10,15,20] } })// select book with price either 10,15 or 20
+        .find()
+        .or([{author : "Mosh"},{isPublished : true}])//parameter is array of objects. If any of the object in array is true it returns true
+        .and([{name: "NodeJs Course"}])//array of filter objects, if all objects are true, it returns true
         .limit(10)
         .sort({name : 1}) //Sorts name => 1 = ascending, -1 = descending
         .select({name : 1, tags : 1}) // selects the feild to display. 1=display,0=don't display
